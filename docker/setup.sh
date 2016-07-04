dea597d51946a8517f25b5242695236e1347fae2
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-07-04 14:39:31 +0200

    Adding example of events/annotation to graphite and example report

--- docker/setup.sh
+++ docker/setup.sh
@@ -3,3 +3,5 @@ sleep 3
 curl -H "Content-Type: application/json" -X POST -d '{"name":"graphite","type":"graphite","url":"http://graphite","access":"proxy","basicAuth":true,"basicAuthUser":"guest","basicAuthPassword":"guest","isDefault":true}' http://admin:admin@grafana:3000/api/datasources
 curl -H "Content-Type: application/json" -X POST -d '{"name":"influxdb","type":"influxdb","url":"http://influxdb:8086","access":"proxy","basicAuth":false,"user":"root","password":"root","isDefault":false}' http://admin:admin@grafana:3000/api/datasources
 curl -H "Content-Type: application/json" -X POST -d '{"name":"prometheus","type":"prometheus","url":"http://prometheus:9090","access":"proxy","basicAuth":false, "isDefault":false}' http://admin:admin@grafana:3000/api/datasources
+
+curl -v -X POST http://guest:guest@graphite/events/ -d '{"what": "This is an example of annotation/event on graphite", "tags" : "test"}'
