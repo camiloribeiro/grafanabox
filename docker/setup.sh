a8a0ed79393fc1a1f84d622a7d76469c1f5f133b
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-07-14 10:51:56 +0200

    yey! 3.1.0 is here

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sleep 3
+sleep 5
 
 curl -H "Content-Type: application/json" -X POST -d '{"name":"graphite","type":"graphite","url":"http://graphite","access":"proxy","basicAuth":true,"basicAuthUser":"guest","basicAuthPassword":"guest","isDefault":true}' http://admin:admin@grafana:3000/api/datasources
 curl -H "Content-Type: application/json" -X POST -d '{"name":"influxdb","type":"influxdb","url":"http://influxdb:8086","access":"proxy","basicAuth":false,"user":"root","password":"root","isDefault":false}' http://admin:admin@grafana:3000/api/datasources
